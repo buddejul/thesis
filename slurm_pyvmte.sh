7171ca6c8390e2b0d27d368ca5b8919516f4fc5a
Julian Budde <buddejul@gmail.com>
2024-10-08 16:47:40 +0200

    Adjust slurm script to debug.

--- slurm_pyvmte.sh
+++ slurm_pyvmte.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=ag_ifs_freyberger
-#SBATCH --partition=intelsr_short
-#SBATCH --time=3:00:00
+#SBATCH --partition=intelsr_debug
+#SBATCH --time=0:15:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=2G
 source ~/.bashrc
 conda deactivate
 conda activate thesis
 
 # Use the dask backend for parallelization
 # https://pytask-parallel.readthedocs.io/en/stable/quickstart.html
-pytask --parallel-backend loky -n 64 -m hpc_pyvmte
+pytask --parallel-backend loky -n 1 -m hpc_pyvmte
