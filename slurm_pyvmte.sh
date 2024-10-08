7631ff5bba93d97c53a5164e2eb0ac9616efa7aa
Julian Budde <buddejul@gmail.com>
2024-10-08 16:49:01 +0200

    Correct queue.

--- slurm_pyvmte.sh
+++ slurm_pyvmte.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=ag_ifs_freyberger
-#SBATCH --partition=intelsr_debug
+#SBATCH --partition=intelsr_devel
 #SBATCH --time=0:15:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
