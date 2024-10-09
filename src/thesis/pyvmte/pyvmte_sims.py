Fix averaging over simulation iterations and add new job.

src/thesis/pyvmte/pyvmte_sims.py
@@ -16,12 +16,11 @@ from pyvmte.utilities import (  # type: ignore[import-untyped]
 )
 from scipy import integrate  # type: ignore[import-untyped]
 
-from thesis.config import RNG
 from thesis.pyvmte.pyvmte_sims_config import Y0_AT, Y0_NT, Y1_AT, Y1_NT
 from thesis.utilities import make_mtr_binary_iv, simulate_data_from_mtrs_binary_iv
 
 
-def simulation_pyvmte(  # noqa: C901, PLR0915, PLR0912
+def simulation_pyvmte(  # noqa: C901, PLR0915, PLR0912, PLR0913
     num_sims: int,
     num_obs: int,
     instrument: Instrument,
@@ -35,6 +34,7 @@ def simulation_pyvmte(  # noqa: C901, PLR0915, PLR0912
     tolerance_est: float,
     u_hi_extra: float,
     true_param_pos: str,
+    rng: np.random.Generator,
 ) -> pd.DataFrame:
     """Perform simulation on the binary IV model using the pyvmte package."""
     _allowed_true_param_pos = ["lower", "upper"]
@@ -226,7 +226,7 @@ def simulation_pyvmte(  # noqa: C901, PLR0915, PLR0912
             mtr0=m0_for_sim,
             mtr1=m1_for_sim,
             num_obs=num_obs,
-            rng=RNG,
+            rng=rng,
             iv_support=instrument.support,
             iv_pmf=instrument.pmf,
             iv_pscores=instrument.pscores,

src/thesis/pyvmte/task_pyvmte_sims.py
@@ -8,7 +8,7 @@ import pytask
 from pytask import Product, task
 from pyvmte.config import IV_SM  # type: ignore[import-untyped]
 
-from thesis.config import BLD
+from thesis.config import BLD, RNG
 from thesis.pyvmte.pyvmte_sims import simulation_pyvmte
 from thesis.utilities import constraint_dict_to_string
 
@@ -91,6 +91,7 @@ class _Arguments(NamedTuple):
     confidence_interval_options: dict = confidence_interval_options
     true_param_pos: str = "lower"
     tolerance_est: float | None = None
+    rng: np.random.Generator = RNG
 
 
 ID_TO_KWARGS = {
@@ -141,6 +142,7 @@ for id_, kwargs in ID_TO_KWARGS.items():
         confidence_interval_options: dict,
         true_param_pos: str,
         tolerance_est: float,
+        rng: np.random.Generator = RNG,
     ) -> None:
         """Perform simulations for the simple model using pyvmte."""
         if tolerance_est is None:
@@ -160,6 +162,7 @@ for id_, kwargs in ID_TO_KWARGS.items():
             confidence_interval_options=confidence_interval_options,
             tolerance_est=tolerance_est,
             true_param_pos=true_param_pos,
+            rng=rng,
         )
 
         res["k_bernstein"] = k_bernstein