052b1e5cba7a75ef6792208ac219fb394fda43cb
Julian Budde <buddejul@gmail.com>
2024-10-29 17:47:51 +0100

    Add cleaned versions of marvin files.

--- copy_from_marvin.sh
+++ copy_from_marvin.sh
@@ -24,10 +24,11 @@ mkdir -p $DIR_TARGET
 # Copy the results from marvin
 scp -r -C $USER@marvin.hpc.uni-bonn.de:/lustre/scratch/data/$USER-thesis/thesis/bld/ $DIR_TARGET
 
-# Now compress
-tar -czf $DIR_TARGET.tar.gz $DIR_TARGET
+# Warning: Compression is not working properly
+# # Now compress
+# tar -czf $DIR_TARGET.tar.gz $DIR_TARGET
 
-echo "[$DATE] Copied results from marvin for job $JOBID"
+# echo "[$DATE] Copied results from marvin for job $JOBID"
 
-# Delete the directory
-rm -r $DIR_TARGET
+# # Delete the directory
+# rm -r $DIR_TARGET
