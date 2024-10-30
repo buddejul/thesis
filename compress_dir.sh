3f4a90d983099e5d5045bb3bcde7e3993fe1507a
Julian Budde <buddejul@gmail.com>
2024-10-30 13:26:44 +0100

    Add marvin jobs on subsampling.

--- compress_dir.sh
+++ compress_dir.sh
@@ -14,9 +14,7 @@ USER=s93jbudd_hpc
 
 DATE=$(date +%d-%m-%Y" "%H:%M:%S)
 
-DIR_SOURCE="./marvin_backup_clean/$JOBID"
-
-DIR_TARGET="./marvin/$JOBID"
+DIR_SOURCE="./marvin/$JOBID"
 
 cd C:/Users/budde/projects/thesis/src/thesis/ || exit
 
@@ -24,6 +22,6 @@ cd C:/Users/budde/projects/thesis/src/thesis/ || exit
 mkdir -p $DIR_SOURCE
 
 # Compress
-tar -czf $DIR_TARGET.tar.gz $DIR_SOURCE
+tar -czf $DIR_SOURCE.tar.gz $DIR_SOURCE
 
 echo "[$DATE] Results compressed: $JOBID"
