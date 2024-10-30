f3b2f59c4f0391cb687b12bc00b7b0c6b35e4a0a
buddejul <buddejul@gmail.com>
2024-10-30 22:15:59 +0100

    Set cores back to 40.

--- slurm_pyvmte.sh
+++ slurm_pyvmte.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=intelsr_medium
 #SBATCH --time=16:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=60
+#SBATCH --cpus-per-task=40
 #SBATCH --mem-per-cpu=400M
 
 # --------------------------------------------------------------------------------------
@@ -40,4 +40,4 @@ conda activate thesis
 
 # Use the dask backend for parallelization
 # https://pytask-parallel.readthedocs.io/en/stable/quickstart.html
-pytask --parallel-backend loky -n 60 -m hpc_pyvmte
+pytask --parallel-backend loky -n 40 -m hpc_pyvmte
